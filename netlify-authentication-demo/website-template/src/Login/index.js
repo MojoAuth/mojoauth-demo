c7d5c0e26499d989f3500c5f872afbab1f797a92
Mehul-Sharma-Git <mehul.sharma@loginradius.com>
2022-04-07 01:53:16 +0530

    fixed warnings

--- netlify-authentication-demo/website-template/src/Login/index.js
+++ netlify-authentication-demo/website-template/src/Login/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import {Button} from 'semantic-ui-react'
 // import MojoAuth from 'mojoauth-web-sdk'
 import config from '../config'
+import 'semantic-ui-css/semantic.min.css'
 const Login = () => {
 
     return(<>
-    <a href={`${config.mojoauth_netlify_url}?redirect_url=${config.redirect_url}`}>
+    <a href={`https://${config.mojoauth_netlify_url}?redirect_url=${config.redirect_url}`}>
     <Button >Login</Button></a>
         
         
