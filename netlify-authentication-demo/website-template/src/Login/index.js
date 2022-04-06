02d151a62c675aa6e79e8ab96f451c4e957da56b
Mehul-Sharma-Git <mehul.sharma@loginradius.com>
2022-04-06 23:44:41 +0530

    initial commit

---
+++ netlify-authentication-demo/website-template/src/Login/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {Button} from 'semantic-ui-react'
+import MojoAuth from 'mojoauth-web-sdk'
+import config from '../config'
+const Login = () => {
+
+    return(<>
+    <a href={`${config.mojoauth_netlify_url}?redirect_url=${config.redirect_url}`}>
+    <Button >Login</Button></a>
+        
+        
+                
+        {/* <div>{JSON.stringify(payload, null, 2)}</div> */}
+
+        </>);
+
+};
+
+export default Login;
