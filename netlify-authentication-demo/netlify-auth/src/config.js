37d173b09ba52e3f108d1792511c43d533b12ac0
Mehul-Sharma-Git <mehul.sharma@loginradius.com>
2022-04-07 17:06:48 +0530

    fixed env issue

--- netlify-authentication-demo/netlify-auth/src/config.js
+++ netlify-authentication-demo/netlify-auth/src/config.js
@@ -1,5 +1,5 @@
 const config ={
-    api_key:process.env.API_KEY || 'APIKEY'
+    api_key:process.env.API_KEY || 'dd69979e-4fba-4445-922d-90f28be17a8e'
 }
 
 export default config
--- netlify-authentication-demo/website-template/src/config.js
+++ netlify-authentication-demo/website-template/src/config.js
@@ -1,7 +1,7 @@
 const config={
-    api_key: process.env.API_KEY  ||'APIKEY', 
-    redirect_url: process.env.REDIRECT_URL || 'REDIRECT_URL',
-    mojoauth_netlify_url: process.env.MOJOAUTH_NETLIFY_URL || 'MOJOAUTH NETLIFY URL'
+    api_key: process.env.API_KEY || 'dd69979e-4fba-4445-922d-90f28be17a8e' ||'APIKEY', 
+    redirect_url: process.env.REDIRECT_URL || 'netlify-auth-website-template.netlify.app/profile' || 'REDIRECT_URL',
+    mojoauth_netlify_url: process.env.MOJOAUTH_NETLIFY_URL || 'mojoauth-netlify-auth.netlify.app' || 'MOJOAUTH NETLIFY URL'
 }
 
 export default config
