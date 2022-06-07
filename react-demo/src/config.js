d1f665ca41df687ff10baa8626ea89301404d1d8
Mehul-Sharma-Git <mehul.sharma@loginradius.com>
2022-06-07 16:11:22 +0530

    added redirect netlify functionality

--- react-demo/src/config.js
+++ react-demo/src/config.js
@@ -1,8 +1,7 @@
 function getConfig() {
 	return {
 		api_key:
-			process.env.REACT_APP_API_KEY ||
-			'bf24525f-c047-401b-ad1e-47bc3d6adaa8',
+			process.env.REACT_APP_API_KEY ,
 			redirect_url:
 				process.env.REACT_APP_REDIRECT_URL || 'http://localhost:3000/dashboard',
 	};
